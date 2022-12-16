62d8dab9e2d7584417e6232e8e739663a5c61085
mwnakahara <85004753+mwnakahara@users.noreply.github.com>
2022-12-16 14:24:20 -0800

    addressed target_blank noreferrer warning

--- src/Footer/Credits.js
+++ src/Footer/Credits.js
@@ -4,11 +4,19 @@ import "./Credits.css";
 export default function Credits() {
   return (
     <div className="Credits">
-      <a href="https://github.com/mwnakahara/Weather-App-React" target="_blank">
+      <a
+        href="https://github.com/mwnakahara/Weather-App-React"
+        target="_blank"
+        rel="noreferrer"
+      >
         Open-source code
       </a>
       {} by {}
-      <a href="https://gallant-colden-8ef37b.netlify.app/" target="_blank">
+      <a
+        href="https://gallant-colden-8ef37b.netlify.app/"
+        target="_blank"
+        rel="noreferrer"
+      >
         MWNakahara
       </a>
     </div>
