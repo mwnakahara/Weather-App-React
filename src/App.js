dead1b5c3d6328f9995ca8f7f83e2204aa340c9b
mwnakahara <85004753+mwnakahara@users.noreply.github.com>
2022-12-16 13:40:44 -0800

    set up react app, imported axios+bootstrap, tested bootstrap

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { ButtonToolbar } from "react-bootstrap";
+import "./App.css";
+//import axios from "axios";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>React Weather App</h1>
+        <p>Let's test this a little further...</p>
+        <a className="btn btn-info">I lead nowhere</a>
       </header>
     </div>
   );
